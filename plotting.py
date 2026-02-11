75193818ef40503281cd2ecf8d3442ed2aefbd04
Elena Bösch <elena.boesch@mabor.ch>
2026-02-11 17:00:55 +0000

    updating dependencies

--- plotting.py
+++ plotting.py
@@ -1,13 +1,68 @@
 import kornia as K
 import kornia.feature as KF
-from kornia_moons.viz import draw_LAF_matches
+# from kornia_moons.viz import draw_LAF_matches
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import matplotlib.patches as mpatches
 import numpy as np
 import torch
 
 from utils import convert_image_to_tensor 
 
+
+# def plot_matches_moons(img_fix, keypts_fix, img_mov, keypts_mov, inliers, N_show: int=100, inliers_only: bool=True, vertical: bool=True):
+#     """
+#     Plots matches between images.
+
+#     Args:
+#         img_fix:    fixed image
+#         keypts_fix: coordinates of keypoints in fixed image 
+#         img_mov:    moving image
+#         keypts_mov: coordinates of keypoints in moving image 
+#         inliers:     array containing classification as inlier of each match
+#         N_show (int):   how many matches to plot 
+#         inliers_only (bool):    whether to plot only matches classified as inliers
+#         vertical (bool):    whether to plot images vertically stacked.
+#     """
+#     # select points to plot
+#     if inliers_only:
+#         inliers_t = torch.as_tensor(inliers).flatten() # flatten inliers array and convert to tensor
+#         inlier_idx = torch.nonzero(inliers_t).flatten() # extract indices of True values
+
+#         N_show = min(N_show, inliers.sum())
+#         rand_idx = torch.randperm(len(inlier_idx))[:N_show]  
+#         show_idx = inlier_idx[rand_idx] # plot subset of inliers
+#     else:
+#         N_show = min(N_show, len(keypts_fix))
+#         show_idx = torch.randperm(len(keypts_fix))[:N_show] # plot subset of *all* matches
+    
+#     keypts_fix_show = keypts_fix[show_idx]
+#     keypts_mov_show = keypts_mov[show_idx]
+#     inliers_show = inliers[show_idx]
+
+#     # plot
+#     fig, ax = draw_LAF_matches(
+#         KF.laf_from_center_scale_ori(
+#             keypts_fix_show.view(1, -1, 2),
+#             torch.ones(keypts_fix_show.shape[0]).view(1, -1, 1, 1),
+#             torch.ones(keypts_fix_show.shape[0]).view(1, -1, 1),
+#         ),
+#         KF.laf_from_center_scale_ori(
+#             keypts_mov_show.view(1, -1, 2),
+#             torch.ones(keypts_mov_show.shape[0]).view(1, -1, 1, 1),
+#             torch.ones(keypts_mov_show.shape[0]).view(1, -1, 1),
+#         ),
+#         torch.arange(keypts_fix_show.shape[0]).view(-1, 1).repeat(1, 2),
+#         K.tensor_to_image(img_fix),
+#         K.tensor_to_image(img_mov),
+#         inliers_show,
+#         draw_dict={"inlier_color": (0.2, 1, 0.2), "tentative_color": None, "feature_color": (0.2, 0.5, 1), "vertical": vertical},
+#         return_fig_ax=True
+#     )
+
+#     return fig, ax
+#     # fig.savefig('LeafAlignment/loftr_test.png')
+#     # fig.show()
 
 def plot_matches(img_fix, keypts_fix, img_mov, keypts_mov, inliers, N_show: int=100, inliers_only: bool=True, vertical: bool=True):
     """
@@ -23,6 +78,28 @@ def plot_matches(img_fix, keypts_fix, img_mov, keypts_mov, inliers, N_show: int=
         inliers_only (bool):    whether to plot only matches classified as inliers
         vertical (bool):    whether to plot images vertically stacked.
     """
+
+    if type(img_fix) == torch.Tensor:
+        # img_fix = K.color.rgb_to_grayscale(img_fix)
+        img_fix = K.tensor_to_image(img_fix)
+    if type(img_mov) == torch.Tensor:
+        # img_mov = K.color.rgb_to_grayscale(img_mov)
+        img_mov = K.tensor_to_image(img_mov)
+
+    if vertical:
+        img_pair = np.concatenate([img_fix, img_mov], axis=0)
+    else:
+        img_pair = np.concatenate([img_fix, img_mov], axis=1)
+
+
+    # Prepare figure
+    if vertical:
+        fig, ax = plt.subplots(figsize=(8,8)) # for vertical plot
+    else:
+        fig, ax = plt.subplots(figsize=(12,6)) # for horizontal plot
+    
+    ax.imshow(img_pair, cmap='gray')
+
     # select points to plot
     if inliers_only:
         inliers_t = torch.as_tensor(inliers).flatten() # flatten inliers array and convert to tensor
@@ -37,31 +114,37 @@ def plot_matches(img_fix, keypts_fix, img_mov, keypts_mov, inliers, N_show: int=
     
     keypts_fix_show = keypts_fix[show_idx]
     keypts_mov_show = keypts_mov[show_idx]
-    inliers_show = inliers[show_idx]
+    inliers_show = inliers[show_idx].astype(float)
 
-    # plot
-    fig, ax = draw_LAF_matches(
-        KF.laf_from_center_scale_ori(
-            keypts_fix_show.view(1, -1, 2),
-            torch.ones(keypts_fix_show.shape[0]).view(1, -1, 1, 1),
-            torch.ones(keypts_fix_show.shape[0]).view(1, -1, 1),
-        ),
-        KF.laf_from_center_scale_ori(
-            keypts_mov_show.view(1, -1, 2),
-            torch.ones(keypts_mov_show.shape[0]).view(1, -1, 1, 1),
-            torch.ones(keypts_mov_show.shape[0]).view(1, -1, 1),
-        ),
-        torch.arange(keypts_fix_show.shape[0]).view(-1, 1).repeat(1, 2),
-        K.tensor_to_image(img_fix),
-        K.tensor_to_image(img_mov),
-        inliers_show,
-        draw_dict={"inlier_color": (0.2, 1, 0.2), "tentative_color": None, "feature_color": (0.2, 0.5, 1), "vertical": vertical},
-        return_fig_ax=True
-    )
+    # set up colormap
+    cmap = cm.winter
+    colors = cmap(inliers_show)
+    
+    # Draw matches
+    if vertical:
+        for (x0, y0), (x1, y1), color in zip(keypts_fix_show, keypts_mov_show, colors):
+            ax.scatter([x0, x1 ], [y0, y1 + img_fix.shape[0]], color=color, s=2)
+            ax.plot([x0, x1 ], [y0, y1+ img_fix.shape[0]], color=color, linewidth=1)
+    else:
+        for (x0, y0), (x1, y1), color in zip(keypts_fix_show, keypts_mov_show, colors):
+            ax.scatter([x0, x1 + img_fix.shape[1]], [y0, y1], color=color, s=1)
+            ax.plot([x0, x1 + img_fix.shape[1]], [y0, y1], color=color, linewidth=1)
 
+    ax.axis('off')
+
+    legend_handles = [
+        mpatches.Patch(color=cmap(0.0), label='Outlier'),
+        mpatches.Patch(color=cmap(1.0), label='Inlier')
+    ]
+
+    if not inliers_only:
+        if vertical:
+            fig.legend(handles=legend_handles, loc='center right', bbox_to_anchor=(1.05, 0.5))#, 0.5, 0.5))
+        else:
+            fig.legend(handles=legend_handles, loc='center right', bbox_to_anchor=(1, 0.5))#, 0.5, 0.5))
+    # plt.tight_layout()
+    
     return fig, ax
-    # fig.savefig('LeafAlignment/loftr_test.png')
-    # fig.show()
 
 
 def plot_matches_conf(img_fix, keypts_fix, img_mov, keypts_mov, confidence, N_show: int=100, vertical: bool=True):
